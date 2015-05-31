83fa97effe530fd014875dbfcbb71dee8696a6e0
Timothy Onggowasito <timong09@gmail.com>
2015-05-31 11:03:58 -0400

    Got jitter to work.

--- src/com/lawlesschickens/mousebot/MBRunnable.java
+++ src/com/lawlesschickens/mousebot/MBRunnable.java
@@ -3,6 +3,7 @@ package com.lawlesschickens.mousebot;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.util.Random;
 
 import javafx.application.Platform;
 
@@ -35,7 +36,7 @@ public class MBRunnable extends Thread{
 				int y = Integer.parseInt(mouseBotUI.table.getColumns().get(2).getCellData(o).toString());
 				//String action = mouseBotUI.table.getColumns().get(3).getCellData(o).toString();
 				float timer = Float.parseFloat(mouseBotUI.table.getColumns().get(4).getCellData(o).toString())*1000;
-				//int jitter = (int)mouseBotUI.table.getColumns().get(5).getCellData(o);
+				int jitter = (int)mouseBotUI.table.getColumns().get(5).getCellData(o);
 				int loop = Integer.parseInt(mouseBotUI.table.getColumns().get(6).getCellData(o).toString());
 				totalCellLoop = loop;
 				for(currentCellLoop = 0; currentCellLoop<totalCellLoop; currentCellLoop++) {
@@ -46,7 +47,7 @@ public class MBRunnable extends Thread{
 							mouseBotUI.table.getSelectionModel().select(o);
 							mouseBotUI.table.getFocusModel().focus(0);
 							try {
-								runRobot(x, y);
+								runRobot(x, y, jitter);
 							} catch (Exception e) {
 								// TODO Auto-generated catch block
 								e.printStackTrace();
@@ -64,7 +65,11 @@ public class MBRunnable extends Thread{
 		}
     }
 	
-    private void runRobot(int x, int y) throws AWTException {
+    private void runRobot(int x, int y, int jitter) throws AWTException {
+    	// Random mouse movement:
+    	Random rn = new Random();
+    	x += rn.nextInt(jitter-0+1)+0;
+    	y += rn.nextInt(jitter-0+1)+0;
     	robot = new Robot();
     	robot.mouseMove(x, y);
     	robot.mousePress(InputEvent.BUTTON1_MASK);
